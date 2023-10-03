14682f1d078d2f12bbb13e269ed8359137d2e4e5
abubakar-ahmed <a.ahmed1@alustudent.com>
2023-10-03 13:02:18 -0700

    tast

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-const readAndPrintFile = (filePath) => {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.log(`An error occurred: ${err.message}`);
-  } else {
-      console.log(data);
-    }
-  });
-};
+const request = require('fs');
 
-if (process.argv.length !== 3) {
-  console.log('Usage: node readFile.js <file_path>');
-} else {
-  const filePath = process.argv[2];
-  readAndPrintFile(filePath);
-}
+const filePath = process.argv[2];
+
+request.readFile(filePath, function (err, data) {
+  if (err) console.log(err);
+  const content = data;
+  console.log(content.toString());
+});

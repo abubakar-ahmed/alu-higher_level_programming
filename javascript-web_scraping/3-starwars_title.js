cb8efa4179d3cb989177ca4232f26bff8257c2bd
abubakar-ahmed <a.ahmed1@alustudent.com>
2023-10-04 07:52:09 -0700

    task

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -5,9 +5,9 @@ const request = require('request');
 const episodeId = process.argv[2];
 
 request.get(`https://swapi-api.alx-tools.com/api/films/${episodeId}`, (err, res) => {
-    if (err) {
-        console.log(err);
-      } else {
-        console.log(JSON.parse(res.body).title);
-      }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(res.body).title);
+  }
 });

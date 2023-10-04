4ad9b7db6ef68396e147f040ec9804b0ca29e5e8
abubakar-ahmed <a.ahmed1@alustudent.com>
2023-10-04 09:00:18 -0700

    task

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -8,13 +8,13 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request.get(url, (err, res) => {
-    if (err) {
-       console.error(err);
-    } else {
-	    fs.writeFile(filePath, res.body, 'utf-8', (err) =>  {
-        if (err) {
- 	 console.log(err)
-            }
-        });
-    } 
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(filePath, res.body, 'utf-8', (err) => {
+      if (err) {
+    console.log(err);
+      }
+    });
+  } 
 });
